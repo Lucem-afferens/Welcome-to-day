d32e7dd8fa9fa4106ff57aceaf76d93ea337a833
1 <a1@MacBook-Air-1.local>
2025-08-06 09:28:20 -0700

    php

--- send.php
+++ send.php
@@ -28,7 +28,7 @@ $firstPrice = trim($_POST['firstPrice'] ?? '');
 // === Промокод и финальная цена === 
 $discountPrice = 990; 
 $discountCode = "katyalegenda"; 
-$price = number_format((strtolower($ad) === $discountCode) ? $discountPrice : floatval($firstPrice), 0, '.', ' ');
+$price = number_format((strtolower($ad) === $discountCode) ? $discountPrice : floatval($firstPrice));
 
 $success = true; 
 $errors = []; 
